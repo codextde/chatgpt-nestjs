3a40ae66f3f83a547d0ead5beb7337ad7ec61bdb
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2023-03-06 17:01:26 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
